18da513f619f75461e79ab2e9ec914e58c00fddb
Ritika Sharma <ritika6401@gmail.com>
2020-09-16 14:35:32 +0530

    Add files via upload

---
+++ Toast-Notificaton/script.js
@@ -0,0 +1,18 @@
+const btn= document.getElementById("btn");
+const container= document.getElementById("container");
+btn.addEventListener("click", () => {
+     createNotification();
+});
+
+function createNotification() {
+const noti = document.createElement("div");
+noti.classList.add("toast");
+
+noti.innerText= "Javascript is awesome";
+
+container.appendChild(noti);
+
+setTimeout(() => {
+    noti.remove();
+},3000);
+}
