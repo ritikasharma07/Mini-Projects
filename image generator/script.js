44af78b2a942abacbbe9eabb5846f4720c81f098
Ritika Sharma <ritika6401@gmail.com>
2020-11-21 00:31:06 +0530

    Add files via upload

---
+++ image generator/script.js
@@ -0,0 +1,6 @@
+function generate(){
+    var image=document.createElement('img');
+    var div= document.getElementById("flex-gen-img");
+    image.src="tenor.gif";
+    div.appendChild(image);
+};
